fdf6986f63d2064bd97158d90b075cd0d49b1f94
Santoshh26 <santoshhsinghh19@gmail.com>
2026-07-27 21:30:37 +0530

    colour changed

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -248,3 +248,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+// ================= PRELOADER =================
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        document.getElementById("preloader").classList.add("hide");
+
+    }, 3000);
+
+});
